0353362b240b699400d7d76dcfcc2af409b33113
Bastien Traverse <neitsab@esrevart.net>
2022-03-10 05:26:52 +0100

    Remove notice function and replace them by simple echo
    It produces bad results and anyway the installation is so fast that they just
    flash by.

--- chroot.sh
+++ chroot.sh
@@ -2,13 +2,7 @@
 
 set -eux
 
-notice() {
-	set +x
-	printf '\e[32m%s\n\e[0m' "$@"
-	set -x
-}
-
-notice "Setting up locale and timezone"
+echo "Setting up locale and timezone"
 #ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 hwclock --systohc
 sed -i '/fr_FR.UTF-8/s/#//' /etc/locale.gen
@@ -16,7 +10,7 @@ locale-gen
 #echo LANG=en_AU.UTF-8 > /etc/locale.conf
 echo "FONT=lat9w-16" > /etc/vconsole.conf
 
-notice "Setting up networking"
+echo "Setting up networking"
 #echo "archvm$(date +%Y%m%d)" > /etc/hostname
 echo "127.0.0.1   localhost
 ::1         localhost
@@ -28,11 +22,11 @@ Name=en*
 [Network]
 DHCP=yes" > /etc/systemd/network/20-wired.network
 
-notice "Setting up NTP"
+echo "Setting up NTP"
 systemctl enable systemd-timesyncd.service
 timedatectl set-ntp true
 
-#notice "Setting up swap"
+#echo "Setting up swap"
 ## TODO: This isn't idempotent, and fails on second run.
 #dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
 #chmod 600 /swapfile
@@ -40,14 +34,14 @@ timedatectl set-ntp true
 #swapon /swapfile
 #echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
-#notice "Installing extra packages"
+#echo "Installing extra packages"
 ## These packages are the minimum needed for rebooting, connecting via SSH, and
 ## git cloning additional setup scripts as a non-root user.
 #pacman --noconfirm -S dhcpcd openssh sudo git
 #systemctl enable dhcpcd.service
 #systemctl enable sshd.service
 
-notice "Configuring initramfs (mkinitcpio) and creating unified kernel image"
+echo "Configuring initramfs (mkinitcpio) and creating unified kernel image"
 # Configure systemd-based initramfs
 sed -i "/^HOOKS/s/base udev/systemd/" /etc/mkinitcpio.conf
 # Configure mkinitcpio to generate a unified kernel image with stub loader
@@ -66,12 +60,12 @@ default_options="--splash /usr/share/systemd/bootctl/splash-arch.bmp"' > /etc/mk
 touch /etc/kernel/cmdline
 mkinitcpio -P
 
-notice "Creating user"
+echo "Creating user"
 NAME=archvm
 useradd -m -G wheel -s /bin/zsh "${NAME}"
 echo "${NAME}":"${NAME}" | chpasswd
 printf "${NAME} ALL=(ALL) ALL\nDefaults timestamp_timeout=10\n" > /etc/sudoers.d/local
 passwd -l root
 
-notice "Creating UEFI boot entry"
+echo "Creating UEFI boot entry"
 efibootmgr  --create --disk /dev/vda --part 1 --label "Arch Linux" --loader /archlinux-linux.efi --verbose
--- install.sh
+++ install.sh
@@ -2,20 +2,14 @@
 
 set -eux
 
-notice() {
-	set +x
-	printf '\e[32m%s\n\e[0m' "$@"
-	set -x
-}
-
-notice "Checking UEFI boot mode"
+echo "Checking UEFI boot mode"
 test -d /sys/firmware/efi/efivars
 
-notice "Updating system clock"
+echo "Updating system clock"
 timedatectl set-timezone Europe/Paris
 timedatectl set-ntp true
 
-notice "Preparing disk"
+echo "Preparing disk"
 echo -e ',512M,U\n,,L' | sfdisk --label gpt /dev/vda
 mkfs.fat -F 32 /dev/vda1
 mkfs.xfs -m bigtime=1,rmapbt=1 /dev/vda2
@@ -23,7 +17,7 @@ mount /dev/vda2 /mnt
 mkdir /mnt/boot
 mount /dev/vda1 /mnt/boot
 
-notice "Fetching chroot script"
+echo "Fetching chroot script"
 # Done as early as possible (after preparing the location to save it to) in
 # case internet connectivity is not working.
 src="https://raw.githubusercontent.com/neitsab/archvm/master/chroot.sh"
@@ -31,7 +25,7 @@ dst="/mnt/chroot.sh"
 curl "$src" > "$dst"
 chmod +x "$dst"
 
-notice "Selecting mirrors"
+echo "Selecting mirrors"
 # Only needed for x86_64, because aarch64 mirrors do GeoIP based balancing. The
 # reflector package doesn't exist for aarch64.
 if [ "$(uname -m)" == x86_64 ]; then
@@ -43,7 +37,7 @@ if [ "$(uname -m)" == x86_64 ]; then
 	          --save /etc/pacman.d/mirrorlist
 fi
 
-notice "Installing base"
+echo "Installing base"
 pacstrap /mnt \
         base linux efibootmgr\
         micro \
@@ -51,7 +45,7 @@ pacstrap /mnt \
         xfsprogs dosfstools \
         sudo
 
-notice "Preparing system with systemd-firstboot"
+echo "Preparing system with systemd-firstboot"
 systemd-firstboot --root=/mnt \
             --locale=fr_FR.UTF-8 \
             --keymap=fr-latin9 \
@@ -60,16 +54,15 @@ systemd-firstboot --root=/mnt \
             --root-shell=/usr/bin/zsh \
             --force \
 
-notice "Set up systemd-resolved symlink"
+echo "Set up systemd-resolved symlink"
 ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 
-notice "Run chroot script"
+echo "Run chroot script"
 arch-chroot /mnt /chroot.sh
 
-notice "Unmounting virtual drive"
+echo "Unmounting virtual drive"
 umount -R /mnt
 
-notice "Shutdown notice"
 echo "Installation complete. You can now:"
 echo "  - Shutdown the VM using \`poweroff\`"
 echo "  - Remove the installation media"
