82f687142478b3b9b4e57d2fa44056455973ec57
Bastien Traverse <neitsab@esrevart.net>
2022-03-11 14:55:46 +0100

    Add options to clear out all partition data and force formatting
    Useful/needed when running the script several times on the same target (or reusing
    an existing install etc.)

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ timedatectl set-timezone Europe/Paris
 timedatectl set-ntp true
 
 echo "Preparing disk"
-sgdisk -n 1:0:+512MiB -t 1:ef00 -n 2:0:0 -t 2:8304 /dev/vda
+sgdisk -o -Z -n 1:0:+512MiB -t 1:ef00 -n 2:0:0 -t 2:8304 /dev/vda
 mkfs.fat -F 32 /dev/vda1
-mkfs.xfs -m bigtime=1,rmapbt=1 /dev/vda2
+mkfs.xfs -f -m bigtime=1,rmapbt=1 /dev/vda2
 mount /dev/vda2 /mnt
 mkdir /mnt/boot
 mount /dev/vda1 /mnt/boot
